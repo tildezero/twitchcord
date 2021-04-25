fe1893acbf9bcb0453b722c09b3465ba375eaa45
zero <suhasvoolla@gmail.com>
2021-04-25 17:35:09 -0500

    actually push the code :bruh:

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const Discord = require('discord.js');
 const tmi = require('tmi.js');
 const keepAlive = require('./server');
-const { SlashCreator, GatewayServer } = require('slash-create');
-const path = require("path")
+const fetch = require('node-fetch');
+require("dotenv").config()
 
 const dclient = new Discord.Client();
 const tclient = new tmi.Client({
 	options: { debug: true },
 	connection: { reconnect: true },
 	identity: {
-		username: 'actuallyaboat',
+		username: process.env.TWITCH_USERNAME,
 		password: process.env.TWITCH_IRC_TOKEN
 	},
-	channels: [ '77zephyr' ]
+	channels: [ process.env.TWITCH_CHANNEL_NAME ]
 });
+const webhookClient = new Discord.WebhookClient(process.env.WEBHOOK_ID, process.env.WEBHOOK_TOKEN);
 
 tclient.connect();
 
@@ -21,39 +22,31 @@ dclient.once('ready', () => {
 	console.log('discord ready');
 });
 
+tclient.on('chat', async (channel, tags, message, self) => {
+  if (self) return;
+  if (!channel == `#${channel}`) return;
+  const resp = await fetch(`https://api.twitch.tv/kraken/users/${tags['user-id']}`, { headers: { "Accept": "application/vnd.twitchtv.v5+json", "Client-ID": process.env.TWITCH_CLIENT_ID } })
+  const res = await resp.json();
+  const avurl = res.logo
+  
+  webhookClient.send(message, {
+    username: `Twitch: ${tags['display-name']}`,
+    avatarURL: avurl,
+    allowedMentions: { parse: [] }
+  })
 
-const webhookClient = new Discord.WebhookClient(process.env.WEBHOOK_ID, process.env.WEBHOOK_TOKEN);
+}); 
 
-tclient.on('message', (channel, tags, message, self) => {
-	// Ignore echoed messages.
-	if(self) return;
-  if (!channel === "#77zephyr") return;
-  if (tags["message-type"] != "chat") return;
-  console.log(`${tags["display-name"]} | ${message}`);
-    const embed = new Discord.MessageEmbed()
-      .setTitle(tags['display-name'])
-      .setDescription(message)
-    webhookClient.send('New message from twitch', {
-	    username: 'twitch bridge',
-	    embeds: [embed],
-    });
 
-});
-
-dclient.on('message', message => {
+dclient.on('message', async message => {
 	if (message.author.id == dclient.user.id) return;
   if (message.webhookID) return;
-
-  if (message.guild.id === "789384477335748668") {
-    if (message.channel.id == "835711648647348244") {
-      tclient.say("77zephyr", `Discord -> ${message.author.tag} | ${message.content}`);
-    }
-  }
+  if (!message.guild.id === process.env.DISCORD_SERVER_ID && message.channel.id == process.env.DISCORD_CHANNEL_ID) return;
+  tclient.say(process.env.TWITCH_CHANNEL_NAME, `Discord -> ${message.author.tag} | ${message.content}`);
 
   if (message.content == "ttv.ping") {
-    message.channel.send(`Pong! ${dclient.ws.ping}`);
+    message.channel.send(`Pong! ${dclient.ws.ping}ms`);
   }
-
 });
 
 keepAlive();
